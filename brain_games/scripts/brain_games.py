5967595de5a69cb047a8d7ca6a9b767e2ec31275
vboxuser <vboxuser@Ubuntic.myguest.virtualbox.org>
2024-08-10 18:36:04 +1000

    stage4_complete

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,15 +1,19 @@
 ﻿#!/usr/bin/env python3
 
-def main(): 
+from brain_games.cli import welcome_user
+
+
+def main():
     print('Welcome to the Brain Games!')
+
 
 if __name__ == '__main__':
     main()
 
-from brain_games.cli import welcome_user
 
 def main():
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
